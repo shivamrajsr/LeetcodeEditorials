b7da77b3111a0de0f2b35dfc0d63fe0fda822aad
shivamrajsr <shivam703368@gmail.com>
2024-06-08 13:46:54 +0530

    string to url

--- src/Editorials/EditorialLinks.java
+++ src/Editorials/EditorialLinks.java
@@ -1,10 +1,20 @@
 package Editorials;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+class EditorialLinks{
 
-public class EditorialLinks{
+    public static final URL gameOfLife;
 
-    public static final String gameOfLife = "https://leetcode.com/problems/game-of-life/solutions/5278189/game-of-life-fastest-beats-100";
-
+    static{
+        try {
+            gameOfLife = new URI("https://leetcode.com/problems/game-of-life/solutions/5278189/game-of-life-fastest-beats-100").toURL();
+            
+        } catch (URISyntaxException | MalformedURLException e) {
+           throw new ExceptionInInitializerError(e);
+        }
+    }
 
 }
