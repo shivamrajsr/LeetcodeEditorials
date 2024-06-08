9d32695fa95fee0a65dd68f00a17ac165836c04e
shivamrajsr <shivam703368@gmail.com>
2024-06-08 13:21:48 +0530

    first commit

---
+++ src/Editorials/EditorialLinks.java
@@ -0,0 +1,10 @@
+package Editorials;
+
+import java.net.URL;
+
+public class EditorialLinks{
+
+    public static final String gameOfLife = "https://leetcode.com/problems/game-of-life/solutions/5278189/game-of-life-fastest-beats-100";
+
+
+}
